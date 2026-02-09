736a76d2b72cf467f3c4df4605f8a37caf293307
Santhosh V <santhoshpersonal26@gmail.com>
2026-02-09 13:46:54 +0000

    v5

--- models/silver/int_payments.sql
+++ models/silver/int_payments.sql
@@ -1,14 +1,17 @@
-{{ config(materialized = 'incremental',
-        incremental_strategy = 'merge',
-        unique_key = 'payment_id'
-)}}
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'merge',
+    unique_key = 'payment_id'
+) }}
 
-select * from 
-{{ ref("stg_payments") }}
+select * from {{ ref("stg_payments") }}
 where payment_id is not null
+
 {% if is_incremental() %}
-    and file_time > (select max(file_time) from {{this}} )
+    and file_time > (select max(file_time) from {{ this }})
 {% endif %}
+
 QUALIFY row_number() over(
-    partition by payment_id order by file_time desc
+    partition by payment_id 
+    order by file_time desc
 ) = 1
